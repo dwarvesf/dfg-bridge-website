3b16d75f64c6f872f2b1113b0a07261d6e168645
j <langdon0003@gmail.com>
2024-04-06 19:39:18 +0700

    fix darkmode css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,15 +9,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@mochi-ui/theme/dist/components/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
+  theme: {},
   plugins: [mochiui(), animate],
 };
 export default config;
