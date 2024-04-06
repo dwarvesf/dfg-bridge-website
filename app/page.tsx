cf7ae02585e33033777fd16e5a0533bf76a03e8c
j <langdon0003@gmail.com>
2024-04-06 19:44:06 +0700

    fix navbar full w

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
-      <div className="z-10 max-w-7xl w-full items-center justify-end mx-auto text-sm flex min-h-12 p-2 bg-slate-300">
+      <div className="z-10 w-full items-center justify-end mx-auto text-sm flex min-h-12 p-2 bg-slate-300">
         <ConnectButton />
       </div>
 
